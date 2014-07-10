32e15d5e65aae1252278940366a1e2bd2c48b89b
Sae <sae@miyake-no-MacBook-Pro.local>
2014-07-11 04:57:45 +0900

    edit : get information from Json

--- python/get_pic_from_flickr.py
+++ python/get_pic_from_flickr.py
@@ -1,16 +1,51 @@
 # -*- coding: utf-8 -*-
-args = { 'method'  : 'flickr.photos.search',
-             'api_key' : '0bacbd797f523d2850bf3b9dd9377c39',           
-             'per_page': '100', # 1回に取得する画像枚数 <= 500
-             'sort'    : 'date-posted-desc',
-             'format'  : 'json',
-             'nojsoncallback' : 1,
-             'place_id' : '9cw_d0JQV7obLp8N5A', # 京都市　IDはflickr.photos.geo.getLocationで見れる
-             'group_id' : '84769930@N00', # kyoto group　サイトで見れる
+import urllib
+import os
+import json
+
+args = { 
+#           'place_id' : '9cw_d0JQV7obLp8N5A', # 京都市　IDはflickr.photos.geo.getLocationで見れる
+#           'group_id' : '84769930@N00', # kyoto group　サイトで見れる
              # 'text'    : text, # 検索ワード
-             'page'    : str(page), # ページ　連続で取得するならこれをインクリメントする
-             'extras'  : "date_upload",
-             'has_geo' : 1  # ジオタグがついているか
-             }
-url = "http://api.flickr.com/services/rest/?%s"%(urllib.urlencode(args) )
-photos = simplejson.loads( urllib.urlopen(url).readline() )
+#           'page'    : str(page), # ページ　連続で取得するならこれをインクリメントする
+            'has_geo' : 1,  # ジオタグがついているか
+            'extras'  : "date_upload",
+            'nojsoncallback' : 1,
+            'format'  : 'json',
+            'per_page': '20',
+            'sort'    : 'date-posted-desc',
+            'api_key' : '0bacbd797f523d2850bf3b9dd9377c39',           
+            'method'  : 'flickr.photos.search'
+
+
+}
+url = "https://api.flickr.com/services/rest/?%s"%(urllib.urlencode(args) )
+photos = json.loads( urllib.urlopen(url).readline() )
+#print  json.dumps(photos, sort_keys= True, indent = 4)
+#print photos
+keylist = photos.keys()
+keylist.sort()
+#print json.dumps(keylist, sort_keys= True, indent=4)
+for k in keylist:
+#      print "[",k,"]"
+      groupdict = photos[k]
+      value=groupdict.keys()
+      for v in value:
+#            print json.dumps(groupdict[v], sort_keys=True, indent=4)
+            a = groupdict[v]
+            if v != "total":
+#at this line v = "photo"
+                  print json.dumps(a, sort_keys=True, indent=4)
+                  for d in range(len(a)):
+                        d = int(d)
+                        print a[d]["farm"]
+                  break
+      break
+#      if value.keys == "photo":
+#            t = value.keys()
+#            print json.dumps(t, sort_keys = True, indent = 4)
+#      print json.dumps(value, sort_keys = True, indent = 4)
+#      valuelist = groupdict.keys()
+#      valuelist.sort()
+#      for value in valuelist:
+#            print groupdict[value]
